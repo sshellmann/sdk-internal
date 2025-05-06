sshellmann/sdk-internal

commit 08722745ba9689df9505591848cd26d4f54c01e9

Format WASM generator code to be in line with other clients (#223)
## 🎟️ Tracking



## 📔 Objective

Reformatting some of my [previous
PR](https://github.com/bitwarden/sdk-internal/pull/197) to be more in
line with the rest of the code

## ⏰ Reminders before review

- Contributor guidelines followed
- All formatters and local linters executed and passed
- Written new unit and / or integration tests where applicable
- Protected functional changes with optionality (feature flags)
- Used internationalization (i18n) for all UI strings
- CI builds passed
- Communicated to DevOps any deployment requirements
- Updated any necessary documentation (Confluence, contributing docs) or
informed the documentation
  team

## 🦮 Reviewer guidelines

<!-- Suggested interactions but feel free to use (or not) as you desire!
-->

- 👍 (`:+1:`) or similar for great changes
- 📝 (`:memo:`) or ℹ️ (`:information_source:`) for notes or general info
- ❓ (`:question:`) for questions
- 🤔 (`:thinking:`) or 💭 (`:thought_balloon:`) for more open inquiry
that's not quite a confirmed
  issue and could potentially benefit from discussion
- 🎨 (`:art:`) for suggestions / improvements
- ❌ (`:x:`) or ⚠️ (`:warning:`) for more significant problems or
concerns needing attention
- 🌱 (`:seedling:`) or ♻️ (`:recycle:`) for future improvements or
indications of technical debt
- ⛏ (`:pick:`) for minor or nitpick changes

crates/bitwarden-generators/src/username.rs
@@ -1,4 +1,5 @@
 use bitwarden_crypto::EFF_LONG_WORD_LIST;
+use bitwarden_error::bitwarden_error;
 use rand::{distributions::Distribution, seq::SliceRandom, Rng, RngCore};
 use reqwest::StatusCode;
 use schemars::JsonSchema;
@@ -7,6 +8,7 @@ use thiserror::Error;
 
 use crate::util::capitalize_first_letter;
 
+#[bitwarden_error(flat)]
 #[derive(Debug, Error)]
 pub enum UsernameError {
     #[error("Invalid API Key")]
@@ -66,6 +68,11 @@ pub enum ForwarderServiceType {
 #[derive(Serialize, Deserialize, Debug, JsonSchema)]
 #[serde(rename_all = "camelCase", deny_unknown_fields)]
 #[cfg_attr(feature = "uniffi", derive(uniffi::Enum))]
+#[cfg_attr(
+    feature = "wasm",
+    derive(tsify_next::Tsify),
+    tsify(into_wasm_abi, from_wasm_abi)
+)]
 pub enum UsernameGeneratorRequest {
     /// Generates a single word username
     Word {

crates/bitwarden-generators/src/generator_client.rs
@@ -1,4 +1,6 @@
 use bitwarden_core::Client;
+#[cfg(feature = "wasm")]
+use wasm_bindgen::prelude::*;
 
 use crate::{
     passphrase::passphrase, password::password, username::username, PassphraseError,
@@ -6,10 +8,12 @@ use crate::{
     UsernameGeneratorRequest,
 };
 
+#[cfg_attr(feature = "wasm", wasm_bindgen)]
 pub struct GeneratorClient {
     client: Client,
 }
 
+#[cfg_attr(feature = "wasm", wasm_bindgen)]
 impl GeneratorClient {
     fn new(client: Client) -> Self {
         Self { client }
@@ -68,7 +72,9 @@ impl GeneratorClient {
     pub fn passphrase(&self, input: PassphraseGeneratorRequest) -> Result<String, PassphraseError> {
         passphrase(input)
     }
+}
 
+impl GeneratorClient {
     /// Generates a random username.
     /// There are different username generation strategies, which can be customized using the
     /// `input` parameter.

crates/bitwarden-wasm-internal/src/client.rs
@@ -4,10 +4,11 @@ use std::fmt::Display;
 use bitwarden_core::{Client, ClientSettings};
 use bitwarden_error::bitwarden_error;
 use bitwarden_exporters::ExporterClientExt;
+use bitwarden_generators::GeneratorClientsExt;
 use bitwarden_vault::VaultClientExt;
 use wasm_bindgen::prelude::*;
 
-use crate::{CryptoClient, GeneratorClient, VaultClient};
+use crate::{CryptoClient, VaultClient};
 
 #[wasm_bindgen]
 pub struct BitwardenClient(pub(crate) Client);
@@ -49,8 +50,8 @@ impl BitwardenClient {
     }
 
     /// Constructs a specific client for generating passwords and passphrases
-    pub fn generator(&self) -> GeneratorClient {
-        GeneratorClient::new(self.0.clone())
+    pub fn generator(&self) -> bitwarden_generators::GeneratorClient {
+        self.0.generator()
     }
 
     pub fn exporters(&self) -> bitwarden_exporters::ExporterClient {

crates/bitwarden-wasm-internal/src/generators.rs
@@ -1,54 +1,0 @@
-use bitwarden_core::Client;
-use bitwarden_generators::{
-    GeneratorClientsExt, PassphraseGeneratorRequest, PasswordGeneratorRequest,
-};
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-pub struct GeneratorClient(Client);
-
-impl GeneratorClient {
-    /// Constructs a new SDK client for generating random passwords and passphrases
-    ///
-    /// # Arguments
-    /// - `client` - The client used to access the SDK
-    ///
-    /// # Returns
-    /// - `Self` - Returns newly constructed client
-    pub fn new(client: Client) -> Self {
-        Self(client)
-    }
-}
-
-#[wasm_bindgen]
-impl GeneratorClient {
-    /// Generates a password from a provided request
-    ///
-    /// # Arguments
-    /// - `request` - Settings for the character sets and password length
-    ///
-    /// # Returns
-    /// - `Ok(String)` containing the generated password
-    /// - `Err(PasswordError)` if password generation fails
-    pub fn password(
-        &self,
-        request: PasswordGeneratorRequest,
-    ) -> Result<String, bitwarden_generators::PasswordError> {
-        self.0.generator().password(request)
-    }
-
-    /// Generates a passphrase from a provided request
-    ///
-    /// # Arguments
-    /// - `request` - Settings for the word count, word separators character sets
-    ///
-    /// # Returns
-    /// - `Ok(String)` containing the generated passphrase
-    /// - `Err(PassphraseError)` if passphrase generation fails
-    pub fn passphrase(
-        &self,
-        request: PassphraseGeneratorRequest,
-    ) -> Result<String, bitwarden_generators::PassphraseError> {
-        self.0.generator().passphrase(request)
-    }
-}

crates/bitwarden-wasm-internal/src/lib.rs
@@ -3,7 +3,6 @@
 mod client;
 mod crypto;
 mod custom_types;
-mod generators;
 mod init;
 mod pure_crypto;
 mod ssh;
@@ -12,6 +11,5 @@ mod vault;
 pub use bitwarden_ipc::wasm::*;
 pub use client::BitwardenClient;
 pub use crypto::CryptoClient;
-pub use generators::GeneratorClient;
 pub use init::init_sdk;
 pub use vault::{folders::FoldersClient, VaultClient};

commit b185bc6e7ec090c68ecd8c130c2772f567890192

fix: export missing WASM types (#263)
## 🎟️ Tracking

<!-- Paste the link to the Jira or GitHub issue or otherwise describe /
point to where this change is coming from. -->

## 📔 Objective

Fixes: errors introduced by
https://github.com/bitwarden/sdk-internal/pull/223

## ⏰ Reminders before review

- Contributor guidelines followed
- All formatters and local linters executed and passed
- Written new unit and / or integration tests where applicable
- Protected functional changes with optionality (feature flags)
- Used internationalization (i18n) for all UI strings
- CI builds passed
- Communicated to DevOps any deployment requirements
- Updated any necessary documentation (Confluence, contributing docs) or
informed the documentation
  team

## 🦮 Reviewer guidelines

<!-- Suggested interactions but feel free to use (or not) as you desire!
-->

- 👍 (`:+1:`) or similar for great changes
- 📝 (`:memo:`) or ℹ️ (`:information_source:`) for notes or general info
- ❓ (`:question:`) for questions
- 🤔 (`:thinking:`) or 💭 (`:thought_balloon:`) for more open inquiry
that's not quite a confirmed
  issue and could potentially benefit from discussion
- 🎨 (`:art:`) for suggestions / improvements
- ❌ (`:x:`) or ⚠️ (`:warning:`) for more significant problems or
concerns needing attention
- 🌱 (`:seedling:`) or ♻️ (`:recycle:`) for future improvements or
indications of technical debt
- ⛏ (`:pick:`) for minor or nitpick changes

crates/bitwarden-generators/src/username.rs
@@ -5,6 +5,8 @@ use reqwest::StatusCode;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
+#[cfg(feature = "wasm")]
+use tsify_next::Tsify;
 
 use crate::util::capitalize_first_letter;
 
@@ -26,6 +28,7 @@ pub enum UsernameError {
 #[derive(Serialize, Deserialize, Debug, JsonSchema)]
 #[serde(rename_all = "camelCase", deny_unknown_fields)]
 #[cfg_attr(feature = "uniffi", derive(uniffi::Enum))]
+#[cfg_attr(feature = "wasm", derive(Tsify), tsify(into_wasm_abi, from_wasm_abi))]
 pub enum AppendType {
     /// Generates a random string of 8 lowercase characters as part of your username
     Random,
@@ -36,6 +39,11 @@ pub enum AppendType {
 #[derive(Serialize, Deserialize, Debug, JsonSchema)]
 #[serde(rename_all = "camelCase", deny_unknown_fields)]
 #[cfg_attr(feature = "uniffi", derive(uniffi::Enum))]
+#[cfg_attr(
+    feature = "wasm",
+    derive(tsify_next::Tsify),
+    tsify(into_wasm_abi, from_wasm_abi)
+)]
 /// Configures the email forwarding service to use.
 /// For instructions on how to configure each service, see the documentation:
 /// <https://bitwarden.com/help/generator/#username-types>